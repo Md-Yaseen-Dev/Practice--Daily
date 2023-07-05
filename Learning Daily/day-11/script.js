9053484164f48be64f1752f4c176f440c9649bc4
mohammedyaseen2211 <ysn6040@gmail.com>
2023-07-05 23:57:52 +0530

    day-11 updated

--- Learning Daily/day-11/script.js
+++ Learning Daily/day-11/script.js
@@ -25,23 +25,45 @@
 
 // unique element
 
-function unique(arr) {
-    let newArr = []
-    for (i = 0; i < arr.length; i++) {
+// function unique(arr) {
+//     let newArr = []
+//     for (i = 0; i < arr.length; i++) {
 
-        for (j = 0; j < arr.length; j++) {
+//         for (j = 0; j < arr.length; j++) {
 
-            if (i !== j && arr[i] == arr[j]) {
-                break;
+//             if (i !== j && arr[i] == arr[j]) {
+//                 break;
+//             }
+//         }
+//         if (j == arr.length) {
+
+//             newArr.push(arr[i])
+//         }
+
+//     }
+//     return newArr
+// }
+
+// console.log(unique([1, 2, 3, 4, 2, 4,3,5]));
+
+
+function repeated(arr) {
+
+    let newArr = [];
+
+    for (i = 1; i < arr.length; i++) {
+
+        for (j = 0; j < i; j++) {
+
+            if (arr[i] == arr[j] && !newArr.find(elem => elem === arr[j])) {
+
+                newArr.push(arr[i]);
             }
         }
-        if (j == arr.length) {
-
-            newArr.push(arr[i])
-        }
-
     }
-    return newArr
-}
 
-console.log(unique([1, 2, 3, 4, 2, 4, 3,]))
+    return newArr;
+}
+console.log(repeated([1, 2, 3, 4, 3, 4, 2, 3, 5]));
+
+
