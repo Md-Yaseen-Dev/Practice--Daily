7164756821641dbcf6be1dc4a5df69ee890e18f7
mohammedyaseen2211 <ysn6040@gmail.com>
2023-07-07 00:36:56 +0530

    daily updated

--- Learning Daily/day-13/script.js
+++ Learning Daily/day-13/script.js
@@ -1,86 +1,174 @@
-// function arrayTarget(arr, target) {
+// // function arrayTarget(arr, target) {
 
-//     let newArr = []
-//     for (i = 1; i < arr.length; i++) {
+// //     let newArr = []
+// //     for (i = 1; i < arr.length; i++) {
 
+
+// //         for (j = 0; j < i; j++) {
+
+// //             if ((arr[i] + arr[j]) == target) {
+
+// //                 newArr.push(arr[j]);
+// //                 newArr.push(arr[i]);
+
+// //             }
+
+// //         }
+// //     }
+// //     if (newArr.length == 0) {
+// //         return -1
+// //     }
+// //     else {
+// //         return newArr
+// //     }
+// // }
+// // console.log(arrayTarget([7, 11, 15, 2, 5, 4], 9));
+
+
+// function pattern() {
+
+//     let str = ""
+//     for (i = 1; i <= 5; i++) {
 
 //         for (j = 0; j < i; j++) {
 
-//             if ((arr[i] + arr[j]) == target) {
+//             str += "* "
 
-//                 newArr.push(arr[j]);
-//                 newArr.push(arr[i]);
+//         }
+
+//         str += "\n"
+
+//     }
+//     return str
+// }
+// console.log(pattern());
+
+// function kite() {
+
+//     let str = ""
+
+//     // upper section
+//     for (i = 1; i <= 6; i++) {
+
+//         for (j = 6; j > i; j--) {
+
+//             str += " "
+
+//         }
+//         for (k = 1; k < i; k++) {
+
+//             str += "* "
+
+//         }
+//         str += "\n"
+//     }
+//     // lower section
+//     for (i = 5; i >= 1; i--) {
+
+//         for (j = 5; j > i; j--) {
+
+//             str += " "
+
+//         }
+
+
+//         for (k = 1; k < i; k++) {
+
+//             str += " *"
+// //
+//       }
+
+//         str += "\n"
+//     }
+
+//     return str
+// }
+// console.log(kite());
+
+
+// let string = "";
+
+// for (let i = 0; i < 5; i++) {
+
+//     for (j = 0; j < 5; j++) {
+
+//         if (i == 0 || i == 4) {
+
+//             string+="*"
+//         }
+//         else{
+//             if(j == 0 || j == 4){
+//                 string+="*"
+
+//             }
+
+//             else{
+//                 string+=" "
 
 //             }
 
 //         }
+        
 //     }
-//     if (newArr.length == 0) {
-//         return -1
-//     }
-//     else {
-//         return newArr
-//     }
+//     string+="\n"
 // }
-// console.log(arrayTarget([7, 11, 15, 2, 5, 4], 9));
+
+// console.log(string)
 
 
-function pattern() {
+// // let string = "";
 
-    let str = ""
-    for (i = 1; i <= 5; i++) {
+// // for (let i = 0; i <= 5; i++) {
 
-        for (j = 0; j < i; j++) {
+// //     for (let k = 0; k < i; k++) {
 
-            str += "* "
+// //         if (i == 5) {
+// //             string += "*"
+// //         }
 
-        }
+// //         else {
 
-        str += "\n"
+// //             if (k == 0 || k == i-1){
+// //                 string += "*"
+// //             }
+// //             else{
+// //                 string+= " ";
+// //             }
+// //         }
+// //     }
+// //     string += "\n"
+// // }
+// // console.log(string)
 
+
+let string = '';
+
+for (let i = 0; i < 5; i++) {
+
+    for (let k = 0; k < i; k++) {
+
+        string += " "
     }
-    return str
+    // for (let j = 2 * (5 - i) - 1; j >= 1; j--) {
+
+    for (let j = 2 * 5 - i - 1; j > i; j--) {
+        string += "*"
+    }
+    string += "\n"
+
 }
-console.log(pattern());
 
-function kite() {
+for (i = 2; i <= 5; i++) {
 
-    let str = ""
+    for (j = 5; j > i; j--) {
 
-    // upper section
-    for (i = 1; i <= 6; i++) {
-
-        for (j = 6; j > i; j--) {
-
-            str += " "
-
-        }
-        for (k = 1; k < i; k++) {
-
-            str += "* "
-
-        }
-        str += "\n"
-    }
-    // lower section
-    for (i = 5; i >= 1; i--) {
-
-        for (j = 5; j > i; j--) {
-
-            str += " "
-
-        }
-
-
-        for (k = 1; k < i; k++) {
-
-            str += " *"
-//
-      }
-
-        str += "\n"
+        string += " "
     }
 
-    return str
+    for (k = 0; k < i * 2 - 1; k++) {
+
+        string += "*"
+    }
+    string += "\n"
 }
-console.log(kite());
+console.log(string)
---
+++ Learning Daily/day-14/script.js
@@ -0,0 +1,117 @@
+// // function numberPattern() {
+
+// //     let str = ""
+// //     for (i = 1; i <= 5; i++) {
+
+
+// //         for (j = 1; j <= i; j++) {
+
+// //             str += j;
+// //         }
+// //         str+="\n"
+// //     }
+// //     return str
+// // }
+
+// // console.log(numberPattern())
+
+
+// // function numberpattern2() {
+
+// //     let str = "";
+// //     let num = 1;
+// //     for (i = 1; i <= 5; i++) {
+
+// //         for (j = 1; j <= i; j++) {
+
+// //             str += num++ + " ";
+
+// //         }
+// //         str += "\n"
+// //     }
+// //     return str;
+// // }
+
+// // console.log(numberpattern2())
+
+
+// // function numberPattern3() {
+
+// //     let str = "";
+
+// //     for (i = 1; i <= 10; i++) {
+
+// //         for (j = 1; j <= 10; j++) {
+
+// //             str += i * j + "  "
+// //         }
+// //         str += "\n"
+// //     }
+// //     return str;
+// // }
+
+// // console.log(numberPattern3());
+
+
+
+function repeated(arr) {
+
+    let newArr = [];
+
+
+    for (i = 1; i < arr.length; i++) {
+
+        for (j = 0; j < i; j++) {
+
+            if (arr[i] == arr[j]) {
+
+                let flag = true;
+
+                if (newArr.length == 0) {
+                    newArr.push(arr[i])
+                }
+
+                for (k = 0; k < newArr.length; k++) {
+
+                    if (newArr[k] !== arr[j]) {
+                        flag = false;
+                    }
+                }
+                if (!flag) {
+                    newArr.push(arr[i])
+                }
+            }
+        }
+    }
+
+    return newArr
+}
+console.log(repeated([1, 2, 3, 4, 3, 4, 2, 3, 5]));
+
+
+// function union(arr1, arr2) {
+
+//     let newArr = []
+
+//     for (i = 0; i < arr1.length; i++) {
+
+//         if (!newArr.includes(arr1[i])) {
+
+//             newArr.push(arr1[i])
+//         }
+//     }
+
+//         for (i = 0; i < arr2.length; i++) {
+
+//             if (!newArr.includes(arr2[i])) {
+
+//                 newArr.push(arr2[i])
+//             }
+
+//     }
+//     return newArr;
+// }
+
+// console.log(union([1, 2, 3, 4], [3, 4, 5]))
+
+
