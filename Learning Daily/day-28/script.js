15d72437ffa2265649e891a0805abf4fe116d5f1
mohammedyaseen2211 <ysn6040@gmail.com>
2023-09-10 22:51:52 +0530

    challenge completed

--- Learning Daily/day-28/script.js
+++ Learning Daily/day-28/script.js
@@ -1,91 +1,24 @@
-// function palindrome(str) {
-//     const arr = [];
-//     let newstr = ""
-//     let str1 = str.toLowerCase();
+function palindrome(str) {
+    const arr = [];
+    let newstr = ""
+    let str1 = str.toLowerCase();
 
-//     for(j=0; j < str1.length; j++){
+    for(j=0; j < str1.length; j++){
 
-//         if (str1[j] >= "a" || str[j] >= 1){
-//            newstr +=  str1[j]
-//            arr.push(str1[j])
-//         }
-// }
-//    return arr
-//   }
+        if (str1[j] >= "a" || str[j] >= 1){
+           newstr +=  str1[j]
+           arr.push(str1[j])
+        }
+}
+   return arr
+  }
 
-//   console.log(palindrome("_eye"));
-// console.log(palindrome("RaceCar"));
-// console.log(palindrome("Race Car"));
-// console.log(palindrome("A man, a plan, a canal, panama"));
-// console.log(palindrome("A man, a plan, a canal, nama"));
+  console.log(palindrome("_eye"));
+console.log(palindrome("RaceCar"));
+console.log(palindrome("Race Car"));
+console.log(palindrome("A man, a plan, a canal, panama"));
+console.log(palindrome("A man, a plan, a canal, nama"));
 
-// function convertToRoman(num) {
-
-//  let arr = []
-//   let str = "";
-//   let num1 = num;
-//  console.log(num1);
-//   for (i = 0; i <= num1; i++) {
-//     arr.push(i)
-//     if (num1 >= 1000) {
-//       str += "M";
-//       arr.push(num1);
-//       num1 = num1 - 1000;
-//     } else if (num1 >= 900 && num1 < 1000) {
-//       str += "CM";
-//       arr.push(num1);
-//       num1 = num1 - 900;
-//     } else if (num1 >= 500) {
-//       str += "D";
-//       arr.push(num1);
-//       num1 = num1 - 500;
-//     } else if (num1 >= 400 && num1 < 500) {
-//       str += "CD";
-//       arr.push(num1);
-//       num1 = num1 - 400;
-//     } else if (num1 >= 100) {
-//       str += "C";
-//       arr.push(num1);
-//       num1 = num1 - 100;
-//     } else if (num1 >= 90 && num1 < 100) {
-//       str += "XC";
-//       arr.push(num1);
-//       num1 = num1 - 90;
-//     } else if (num1 >= 50) {
-//       str += "L";
-//       arr.push(num1);
-//       num1 = num1 - 50;
-//     }
-//     else if (num1 >= 40 && num1 < 50) {
-//       str += "XL";
-//       arr.push(num1);
-//       num1 = num1 - 40;
-//     } else if (num1 >= 10 ) {
-//       str += "X";
-//       arr.push(num1);
-//       num1 = num1 - 10;
-//     } else if (num1 == 9) {
-//       str += "IX";
-//       arr.push(num1);
-//       num1 = num1 - 9;
-//     } else if (num1 >= 5) {
-//       str += "V";
-//       arr.push(num1);
-//       num1 = num1 - 5;
-//     } else if (num1 == 4) {
-//       str += "IV";
-//       arr.push(num1);
-//       num1 = num1 - 4;
-//     } else if (num1 > 0) {
-//       str += "I";
-//       arr.push(num1);
-//       num1 = num1 - 1;
-//     }
-//   }
-//   return str;
-// }
-// console.log(convertToRoman(49));
-// console.log(convertToRoman(149));
 
 // --------------------------------------------
 // function rot13(str) {
